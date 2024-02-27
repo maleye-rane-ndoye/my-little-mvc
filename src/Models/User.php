6392c46e218223eed3f3685e7d64a66b6fae7579
maleye rane <maleyeranendoye@Mydebian.lan>
2024-02-27 04:31:42 +0100

    password hashed

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,22 +4,40 @@ namespace App\Models;
 
 use PDOException;
 
-class User extends Databaselog {
-
-
+class User extends DatabaseLog{
 
 
     public function register($firstname, $lastname, $email, $password) {
         try {
             $pdo = $this->getBdd();
-            $stmt = $pdo->prepare("INSERT INTO User (firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$firstname, $lastname, $email, $password]);
-            return true; // ou une autre indication de succès
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hashage du mot de passe
+            $query = "INSERT INTO User (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$firstname, $lastname, $email, $hashedPassword]);
+            return true; // Succès de l'insertion
         } catch (PDOException $e) {
-            // Gérer les erreurs d'insertion
-            return false; // ou lancer une exception, etc.
+            // Gérer les erreurs de requête SQL
+            echo "Erreur SQL : " . $e->getMessage() . "<br>";
+            echo "Requête SQL : " . $query . "<br>";
+            echo "Paramètres : " . implode(", ", [$firstname, $lastname, $email, $password]) . "<br>";
+            return false; // Échec de l'insertion
         }
     }
+    
+
+    public function emailExists($email) {
+        try {
+            $pdo = $this->getBdd();
+            $stmt = $pdo->prepare("SELECT COUNT(*) FROM User WHERE email = ?");
+            $stmt->execute([$email]);
+            $count = $stmt->fetchColumn();
+            return $count > 0;
+        } catch (PDOException $e) {
+            // Gérer les erreurs de requête SQL
+            return false;
+        }
+    }
+    
 
     public function login($email, $password) {
         try {
