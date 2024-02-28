1b3f3ed5ff5ff5d7318712adb4108f5721fe5cab
maleye rane <maleyeranendoye@Mydebian.lan>
2024-02-28 04:46:19 +0100

    file to manage a session user yet authentifyed

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -1,15 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Récupérer le formulaire d'inscription par son ID
-    var registerForm = document.getElementById("registerForm");
+function redirectToLogin() {
+    if (!document.getElementById("registerForm")) {
+        return; // Quitter la fonction si le formulaire n'est pas présent sur la page
+    }
+    setTimeout(function() {
+        window.location.href = '/B2/my-little-mvc/login';
+    }, 2000);
+}
 
-    // Écouter l'événement de soumission du formulaire
-    registerForm.addEventListener("submit", function(event) {
-        // Empêcher le comportement par défaut du formulaire (rechargement de la page)
-        event.preventDefault();
-
-        // Effectuer une action après la soumission du formulaire
-        setTimeout(function() {
-            window.location.href = '/B2/my-little-mvc/login';
-        }, 2000);
-    });
-});
+// Appeler la fonction de redirection
+redirectToLogin();
---
+++ src/config/session_manager.php
@@ -0,0 +1,18 @@
+<?php
+// session.php
+
+// Démarrez la session
+session_start();
+
+
+// Vérifiez l'authentification de l'utilisateur dans d'autres scripts
+if (isset($_SESSION['user_id'])) {
+    // L'utilisateur est connecté
+    $userId = $_SESSION['user_id'];
+    $username = $_SESSION['username'];
+    // Vous pouvez utiliser ces informations pour personnaliser l'expérience de l'utilisateur
+} else {
+    // L'utilisateur n'est pas connecté, vous pouvez rediriger vers la page de connexion
+    header("Location: /B2/my-little-mvc/login");
+    exit();
+}
